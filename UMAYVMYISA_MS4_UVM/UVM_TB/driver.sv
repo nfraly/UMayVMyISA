9579533e1495d22db9d3b33a5d127e32491d6348
Nathan Fraly <nfraly85@gmail.com>
2026-02-23 20:23:26 -0800

    add driver/monitor skeleton

---
+++ UMAYVMYISA_MS4_UVM/UVM_TB/driver.sv
@@ -0,0 +1,33 @@
+class driver extends uvm_driver;
+    `uvm_component_utils (driver);
+
+    function new (string name = "driver", uvm_component parent = null);
+        super.new (name, parent);
+    endfunction
+
+    virtual intf vif;
+
+    virtual function void build_phase (uvm_phase phase);
+        super.build_phase (phase);
+        if (!uvm_config_db #(vif)::get(this, null, "vif" vif)) begin
+            `uvm_fatal(get_type_name(), "Didn't get handle to interface vif");
+        end
+    endfunction
+
+    virtual task run_phase (uvm_phase phase);
+        reg_item testObj;
+        super.run_phase(phase);
+
+        forever begin
+            `uvm_info (get_type_name(), $sformatf ("Waiting for data from sequencer", UVM_LOW);
+            seq_item_port.get_next_item (testObj);
+            drive_item (testObj);
+            seq_item_port.item_done();
+        end
+    endtask
+
+    virtual task drive_item (reg_item testObj);
+        //Use bus protocol to drive
+    endtask
+
+endclass
---
+++ UMAYVMYISA_MS4_UVM/UVM_TB/monitor.sv
@@ -0,0 +1,32 @@
+class monitor extends uvm_monitor;
+    `uvm_component_utils (monitor);
+
+    virtual intf vif;
+
+    uvm_analysis_port #(reg_item) mon_analysis_port;
+
+    function new (string name = "monitor", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+    virtual function void build_phase (uvm_phase phase);
+        super.build_phase(phase);
+
+        mon_analysis_port = new("mon_analaysis_port", this);
+
+        if (!uvm_config_db #(virtual intf)::get(this, null, "vif" vif)) begin
+            `uvm_fatal (get_type_name(), "Monitor interface not in db");
+        end
+    endfunction
+
+    virtual task run_phase (uvm_phase phase);
+        reg_item testObj = reg_item::type_id::create("testObj", this);
+        forever begin
+            @(posedge vif.clk); //whatever signifies a new event
+            testobj.members = vif.members; //grab the DUT output
+            //do some coverage stuff here?
+
+            mon_analysis_port.write(testObj);
+        end
+    endtask
+endclass
