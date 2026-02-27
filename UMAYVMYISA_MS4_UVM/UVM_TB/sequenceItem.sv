fb6e49814173a36eed7625236bf53d838c71b3e7
Nathan Fraly <nfraly85@gmail.com>
2026-02-27 13:49:08 -0800

    placeholder for instruction constraints

--- UMAYVMYISA_MS4_UVM/UVM_TB/sequenceItem.sv
+++ UMAYVMYISA_MS4_UVM/UVM_TB/sequenceItem.sv
@@ -1,7 +1,9 @@
 import system_widths_pkg::*;
 class trace extends uvm_sequence_item;
-    rand logic [31:0] instruction;
+    rand logic [27:0] payload;
+    rand logic [3:0] opCode;
     rand logic [corewidth-1:0] targetCore;
+    logic [31:0] instruction;
     //rand logic [ADDR_W-1:0] addr;
     //rand logic [DATA_W-1:0] wdata;
     //rand int unsigned core_id;
@@ -12,12 +14,25 @@ class trace extends uvm_sequence_item;
     //bit mem_done;
 
     constraint validCore {targetCore inside {[0:CORES]};}
-    constraint validInstruction {instruction[3:0] inside{[0:13]};
-                                 instruction[8:4] inside{[0:31]};
-                                 instruction[13:9] inside {[0:31]};
-                                 instruction [31:14] inside {[0:128]};//what are these bits for?
-
-    }
+    constraint validInstruction {opCode[3:0] inside{[0:13]};}
+    //constraint validPayload {
+    //    unique case (opCode)
+    //        4'b0000:
+    //        4'b0001:
+    //        4'b0010:
+    //        4'b0011:
+    //        4'b0100:
+    //        4'b0101:
+    //        4'b0110:
+    //        4'b0111:
+    //        4'b1000:
+    //        4'b1001:
+    //        4'b1010:
+    //        4'b1011:
+    //        4'b1100:
+    //        4'b1101:
+    //        4'b1110:
+    //        4'b1111:
 
     `uvm_object_utils_begin(trace)
         `uvm_field_int(instruction, UVM_DEFAULT)
@@ -31,4 +46,6 @@ class trace extends uvm_sequence_item;
     function new(string name = "trace");
         super.new(name);
     endfunction
+
+    function isMem
 endclass
