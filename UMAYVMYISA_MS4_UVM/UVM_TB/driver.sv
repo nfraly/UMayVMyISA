ceed02138d441ef120dd025643ae1062d4814feb
Nathan Fraly <nfraly85@gmail.com>
2026-03-11 11:19:52 -0700

    Add proper handshake

--- UMAYVMYISA_MS4_UVM/UVM_TB/driver.sv
+++ UMAYVMYISA_MS4_UVM/UVM_TB/driver.sv
@@ -4,21 +4,20 @@ class driver extends uvm_driver #(trace#(3));
     localparam logic [3:0] ADD = 4'b0001;
 
     virtual intf vif;
-    //sequenceitem
     trace#(3) testObj;
 
 
 
     function new (string name = "driver", uvm_component parent = null);
         super.new (name, parent);
-        `uvm_info("DRIVER", "Constructing Driver", UVM_HIGH)
+        `uvm_info("DRIVER", "Constructing Driver", UVM_LOW)
     endfunction
 
 
     function void build_phase (uvm_phase phase);
         super.build_phase (phase);
         testObj = trace#(3)::type_id::create("testObj");
-        `uvm_info("DRIVER", "Driver Build_phase", UVM_HIGH)
+        `uvm_info("DRIVER", "Driver Build_phase", UVM_LOW)
         if(!(uvm_config_db #(virtual intf)::get(this, "*", "vif", vif))) begin
             `uvm_error("DRIVER", "Failed to get VIF from config DB")
         end
@@ -26,33 +25,31 @@ class driver extends uvm_driver #(trace#(3));
 
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
-            `uvm_info("DRIVER", "Driver Connect_phase", UVM_HIGH)
+            `uvm_info("DRIVER", "Driver Connect_phase", UVM_LOW)
     endfunction
 
     virtual task run_phase (uvm_phase phase);
         super.run_phase(phase);
-        `uvm_info("DRIVER", "Driver Run_phase", UVM_HIGH)
+        `uvm_info("DRIVER", "Driver Run_phase", UVM_LOW)
 
 
         forever begin
-            `uvm_info ("DRIVER", $sformatf ("Waiting for data from sequencer"), UVM_LOW)
+            `uvm_info ("DRIVER", $sformatf ("Waiting for data from sequencer"), UVM_HIGH)
             seq_item_port.get_next_item (testObj);
-            `uvm_info ("DRIVER", $sformatf ("About to drive an item"), UVM_LOW)
+            `uvm_info ("DRIVER", $sformatf ("About to drive an item"), UVM_HIGH)
             drive_item (testObj);
-            `uvm_info ("DRIVER", $sformatf ("Drove an item"), UVM_LOW)
+            `uvm_info ("DRIVER", $sformatf ("Drove an item"), UVM_HIGH)
             seq_item_port.item_done();
         end
         `uvm_info("DRIVER", "Driver Run_phase Complete", UVM_HIGH)
     endtask
 
     task drive_item (trace#(3) testObj);
-        `uvm_info ("DRIVER", $sformatf ("Waiting to drive an item"), UVM_LOW)
-        wait(vif.instr_ready);
-        `uvm_info ("DRIVER", $sformatf ("Driving an item"), UVM_LOW)
+        `uvm_info ("DRIVER", $sformatf ("Driving an item"), UVM_HIGH)
         vif.instr_core_sel = testObj.targetCore;
         vif.instr_word = testObj.instruction;
         vif.instr_valid = 1'b1;
-        @(posedge vif.clk); vif.instr_valid = 1'b0;
+        wait(vif.instr_ready); vif.instr_valid = 1'b0;
     endtask
 
 endclass
