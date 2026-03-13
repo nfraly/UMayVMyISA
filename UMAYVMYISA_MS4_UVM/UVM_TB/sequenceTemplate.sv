682036329e320895d51fd682eb8d1e1d27ae4e48
Nathan Fraly <nfraly85@gmail.com>
2026-03-12 17:44:48 -0700

    add memory based test cases

--- UMAYVMYISA_MS4_UVM/UVM_TB/sequenceTemplate.sv
+++ UMAYVMYISA_MS4_UVM/UVM_TB/sequenceTemplate.sv
@@ -1,5 +1,5 @@
 class <className> #(parameter CORES = 3) extends uvm_sequence#(trace#(3));
-    `uvm_object_utils(<className>#(3)
+    `uvm_object_utils(<className>#(3))
     localparam corewidth = $clog2(CORES);
     function new(string name = "<className>");
         super.new(name);
